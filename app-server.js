8f9a58829be798589a8c7a82a19655af8ec7f6d3
kutsaniuk <kutsaniuk@gmail.com>
2017-12-28 18:04:16 +0200

    Changed engines

--- app-server.js
+++ app-server.js
@@ -1,22 +1,21 @@
-
-// app-server.js
 var express = require('express');
 var bodyParser = require('body-parser');  
 var stripe = require("stripe")(process.env.STRIPE_SECRET || "sk_test_LFGQnyARiQMR5pAm01cMhsfB");
 var app = express();
-app.set('port', process.env.PORT || 3000)
-app.use(express.static(__dirname))
-app.use(bodyParser.json())
-var http = require('http').Server(app)
-// Route
+
+app.set('port', process.env.PORT || 3000);
+app.use(express.static(__dirname));
+app.use(bodyParser.json());
+
+var http = require('http').Server(app);
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 app.post('/charge', (req, res) => {
     var token = req.body.stripeToken;
 
-    // Charge the user's card:
-    var charge = stripe.charges.create({
+    stripe.charges.create({
       amount: req.body.amount,
       currency: "usd",
       description: req.body.amount.description,
@@ -30,15 +29,18 @@ app.post('/contact', (req, res) => {
     var data = req.body;
     var api_key = process.env.MAILGUN_KEY || 'key-333dec2f4a33beb43527c09b0c028fa7';
     var domain = process.env.MAILGUN_DOMAIN || 'sandbox02c1bc1aa4ad46ab806ab03293fd49b2.mailgun.org';
+    
     if (!api_key || !domain)
         return res.status(500).send({ "status": "error", "message": "You must add a MailGun api key and domain using environment variables located in Your Cosmic JS Bucket > Deploy to Web.  Contact your developer to add these values." });
-    var mailgun = require('mailgun-js')({ apiKey: api_key, domain: domain })
+    
+    var mailgun = require('mailgun-js')({ apiKey: api_key, domain: domain });
     var mailgun_data = {
         from: data.email,
         to: process.env.WORK_EMAIL || 'kutsaniuk@gmail.com',
         subject: 'Contact',
         text: data.text + '\n\n' + data.name
     };
+    
     mailgun.messages().send(mailgun_data, function (error, body) {
         if (error)
             return res.status(500).send({ "status": "error", "message": "You must add a MailGun api key and domain using environment variables located in Your Cosmic JS Bucket > Deploy to Web.  Contact your developer to add these values." });
@@ -46,6 +48,7 @@ app.post('/contact', (req, res) => {
         return res.status(200).send({"status": "success"});
     })
 });
+
 http.listen(app.get('port'), () => {
   console.log('Ecommerce App listening on ' + app.get('port'))
 });
